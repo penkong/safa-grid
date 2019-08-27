23f33b1615520113ee61fdc6da6b2797731023cd
mk-nazemi <nazemi.works@gmail.com>
2019-08-27 11:24:36 +0430

    add new style for deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-// import NiceHandsomeButton from "./NiceHandsomeButton.vue";
+// Import vue component
 import SafaGridView from "./SafaGridView.vue";
 import Sth from "./Sth.vue";
 
+// export default {
+//     install(Vue, options) {
+//         Vue.component("safa-grid-view", SafaGridView);
+//         Vue.component("sth", Sth);
+//     }
+// };
+
+
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component('safa-grid-view', SafaGridView);
+    Vue.component('sth', Sth);
+}
+
+// Create module definition for Vue.use()
+const plugin = {
+    install,
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+// To allow use as module (npm/webpack/etc.) export component
 export default {
-    install(Vue, options) {
-        Vue.component("safa-grid-view", SafaGridView);
-        Vue.component("sth", Sth);
-    }
+    SafaGridView,
+    Sth
 };
